f12477f2e4189e25fc7a4771affea6b254497583
Jan ATAC <can.atac@gmail.com>
2024-01-05 13:14:32 +0000

    to test

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/canatac/effective-umbrella
 go 1.21.5
 
 require (
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/mailjet/mailjet-apiv3-go v0.0.0-20201009050126-c24bc15a9394
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
-	github.com/go-redis/redis/v8 v8.11.5 // indirect
-	github.com/mailjet/mailjet-apiv3-go v0.0.0-20201009050126-c24bc15a9394 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,85 @@
 package main
 
 import (
-    "encoding/json"
-    "net/http"
-	"github.com/mailjet/mailjet-apiv3-go"
+	"context"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"regexp"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	mailjet "github.com/mailjet/mailjet-apiv3-go"
 )
+
+var dbUrl = os.Getenv("DB_URL")
+var dbPort = os.Getenv("DB_PORT")
+var mailServerApiKey = os.Getenv("MAIL_SERVER_API_KEY")
+var mailServerApiSecret = os.Getenv("MAIL_SERVER_API_SECRET")
+var fromEmail = os.Getenv("FROM_EMAIL")
+var serverPort = os.Getenv("SERVER_PORT")
+
+var ctx = context.Background()
+var emailRegex = regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+
+func connectToRedis(redisAddr, redisPassword string) *redis.Client {
+    client := redis.NewClient(&redis.Options{
+        Addr:     redisAddr,
+        Password: redisPassword, // no password set
+        DB:       0,  // use default DB
+    })
+
+    _, err := client.Ping(ctx).Result()
+    if err != nil {
+        panic(err)
+    }
+
+    return client
+}
+
+func generateOTP() string {
+    rand.New(rand.NewSource(time.Now().UnixNano()))
+    otp := rand.Intn(1000000) // generate a random number between 0 and 999999
+    return fmt.Sprintf("%06d", otp) // format the OTP as a 6-digit number
+}
+
+func storeOTPInRedis(redisClient *redis.Client, email, otp string) error {
+    err := redisClient.Set(ctx, email, otp, 5*time.Minute).Err() // store the OTP in Redis, with an expiration time of 5 minutes
+    return err
+}
 
 type Request struct {
     ApiKey string `json:"apiKey"`
     Email  string `json:"email"`
+}
+
+
+func sendEmail(apiKey, apiSecret, fromEmail, toEmail, subject, textContent string) error {
+    mailjetClient := mailjet.NewMailjetClient(apiKey, apiSecret)
+
+    messagesInfo := []mailjet.InfoMessagesV31 {
+        {
+          From: &mailjet.RecipientV31{
+            Email: fromEmail,
+            Name: "Misfits Pilot",
+          },
+          To: &mailjet.RecipientsV31{
+            mailjet.RecipientV31 {
+              Email: toEmail,
+              Name: "passenger 1",
+            },
+          },
+          Subject: subject,
+          TextPart: textContent,
+          HTMLPart: "<h3>Dear passenger 1, welcome to <a href=\"https://certeef.misfits.fr/\">Certeef</a>!</h3><br />May the delivery force be with you!",
+        },
+      }
+
+    messages := mailjet.MessagesV31{Info: messagesInfo }
+    _, err := mailjetClient.SendMailV31(&messages)
+    return err
 }
 
 func main() {
@@ -20,45 +91,30 @@ func main() {
 
         var req Request
         err := json.NewDecoder(r.Body).Decode(&req)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusBadRequest)
+        if len(req.ApiKey) != 32 {
+            http.Error(w, "Invalid API key", http.StatusBadRequest)
             return
         }
 
-        // TODO: Vérifier la clé API et le format de l'e-mail
-		
-        // TODO: Générer un OTP et l'envoyer par e-mail
+        if !emailRegex.MatchString(req.Email) {
+            http.Error(w, "Invalid email format", http.StatusBadRequest)
+            return
+        }
 
-		err = sendEmail("your-mailjet-api-key", "your-mailjet-api-secret", "your-email@example.com", req.Email, "Your OTP", "Your OTP is 123456")
+        redisClient := connectToRedis(fmt.Sprintf("%s:%s",dbUrl,dbPort), "")
+        otp := generateOTP()
+        err = storeOTPInRedis(redisClient, req.Email, otp)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+		err = sendEmail(mailServerApiKey, mailServerApiSecret, fromEmail, req.Email, "Your OTP", fmt.Sprintf("Your OTP is %s", otp))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
     })
 
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(fmt.Sprintf(":%s",serverPort), nil)
 }
 
-func sendEmail(apiKey, apiSecret, fromEmail, toEmail, subject, textContent string) error {
-    mailjetClient := mailjet.NewMailjetClient(apiKey, apiSecret)
-
-    email := &mailjet.MessageV31{
-        From: &mailjet.RecipientV31{
-            Email: fromEmail,
-            Name:  "Your Name",
-        },
-        To: &mailjet.RecipientsV31{
-            mailjet.RecipientV31{
-                Email: toEmail,
-                Name:  "John",
-            },
-        },
-        Subject:  subject,
-        TextPart: textContent,
-        CustomID: "AppGettingStartedTest",
-    }
-
-    messages := mailjet.MessagesV31{Info: []mailjet.InfoMessagesV31{*email}}
-    _, err := mailjetClient.SendMailV31(&messages)
-    return err
-}
